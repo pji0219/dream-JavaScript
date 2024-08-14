cc8ebf7e6df57862a1eb3456848c156435e70a4e
pji0219 <pji219@naver.com>
2024-08-15 02:13:52 +0900

    spread

--- 9.iterable/5.spread.js
+++ 9.iterable/5.spread.js
@@ -11,3 +11,27 @@ function add(a, b, c) {
 
 const num = [1, 2, 3];
 console.log(add(...num));
+
+// Rest parameters
+function sum(first, second, ...nums) {
+  console.log(nums);
+}
+sum(1, 2, 0, 1, 2, 3);
+
+// Array concat
+const fruits1 = ['🍑', '🍋'];
+const fruits2 = ['🍅', '🍒'];
+let arr = fruits1.concat(fruits2);
+console.log(arr);
+arr = [...fruits1, '🥦', ...fruits2];
+console.log(arr);
+
+// Object
+const pji = { name: 'pji', age: 20, home: { address: 'home' } };
+const updated = {
+  ...pji,
+  job: 's/w engineer',
+};
+console.log(pji);
+
+console.log(updated);
