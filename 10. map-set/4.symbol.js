1f25cf0b55e079cf6f1549f77e3b895e83e3eb0e
pji0219 <pji219@naver.com>
2024-11-07 23:30:30 +0900

    symbol

--- 10. map-set/4.symbol.js
+++ 10. map-set/4.symbol.js
@@ -9,3 +9,12 @@ map.set(key1, 'Hello');
 
 console.log(map.get(key2));
 console.log(key1 === key2);
+
+// 동일한 이름으로 하나의 키를 사용하고 싶다면, Symbol.for
+// 전역 심볼 레지스트리(Global Symbol Registry)
+const k1 = Symbol.for('key');
+const k2 = Symbol.for('key');
+console.log(k1 === k2);
+
+console.log(Symbol.keyFor(k1));
+console.log(Symbol.keyFor(key1));
